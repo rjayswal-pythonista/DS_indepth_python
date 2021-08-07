ed15f76e3817f3cdf4d28ca61eaf0df30979def7
rjayswal-pythonista <roshanjayswal15@cisco.com>
2021-08-07 23:33:19 +0530

    DS indepth Python

---
+++ BinarySearchTree.py
@@ -0,0 +1,47 @@
+class _Node:
+    __slots__ = '_element', '_left', '_right'
+
+    def __init__(self, element, left=None, right=None):
+        self._element = element
+        self._left = left
+        self._right = right
+
+class BinarySearchTree:
+    def __init__(self):
+        self._root = None
+
+    def insert(self,troot,e):
+        temp = None
+        while troot:
+            temp = troot
+            if e == troot._element:
+                return
+            elif e < troot._element:
+                troot = troot._left
+            elif e > troot._element:
+                troot = troot._right
+        n = _Node(e)
+        if self._root:
+            if e < temp._element:
+                temp._left = n
+            else:
+                temp._right = n
+        else:
+            self._root = n
+
+    def inorder(self,troot):
+        if troot:
+            self.inorder(troot._left)
+            print(troot._element,end=' ')
+            self.inorder(troot._right)
+
+B = BinarySearchTree()
+B.insert(B._root,50)
+B.insert(B._root,30)
+B.insert(B._root,80)
+B.insert(B._root,10)
+B.insert(B._root,40)
+B.insert(B._root,60)
+B.insert(B._root,90)
+B.inorder(B._root)
+
